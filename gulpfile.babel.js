f6d4ecfd427e65687c4bd95e5250ce8d5f74c546
Th3Tou7 <elbasiriy@gmail.com>
2020-05-15 21:31:52 +0000

    add server configs, packages file and edit gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,112 @@
+import gulp from "gulp";
+import babel from "gulp-babel";
+import sourcemaps from "gulp-sourcemaps";
+import sass from "gulp-sass";
+import uglify from "gulp-uglify";
+import del from "del";
+import rename from "gulp-rename";
+import livereload from "gulp-livereload";
+import autoprefixer from "gulp-autoprefixer";
+import imagemin from "gulp-imagemin";
+import htmlmin from "gulp-htmlmin";
+
+//define paths
+const paths = {
+  style: {
+    src: "src/styles/style.scss",
+    dest: "dist/styles/",
+  },
+  script: {
+    src: "src/scripts/index.js",
+    dest: "dist/scripts/",
+  },
+  html: {
+    src: "src/*.html",
+    dest: "dist",
+  },
+  imgs: {
+    src: "src/imgs/*.{jpg,jpeg,png,svg}",
+    dest: "dist/imgs",
+  },
+};
+
+const clean = () => del(["dest"]);
+
+const script = () => {
+  return gulp
+    .src(paths.script.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(
+      rename({
+        basename: "sctipt",
+        suffix: ".min",
+        extname: ".js",
+      })
+    )
+    .pipe(gulp.dest(paths.script.dest))
+    .pipe(livereload());
+};
+
+const style = () => {
+  return gulp
+    .src(paths.style.src)
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(
+      rename({
+        basename: "style",
+        suffix: ".min",
+        extname: ".css",
+      })
+    )
+    .pipe(gulp.dest(paths.style.dest))
+    .pipe(livereload());
+};
+
+const html = () => {
+  return gulp
+    .src(paths.html.src)
+    .pipe(
+      rename({
+        basename: "index",
+        extname: ".html",
+      })
+    )
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(livereload());
+};
+
+const imgs = () => {
+  return gulp
+    .src(paths.imgs.src)
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest(paths.imgs.dest))
+    .pipe(livereload());
+};
+
+const watch = () => {
+  require("./server");
+  //to reload automatically, you have to add a script tag in your html file with src of  'http://localhost:35729/livereload.js'
+  livereload.listen();
+  gulp.watch(paths.script.src, script);
+  gulp.watch("src/styles/**/*.scss", style);
+  gulp.watch(paths.html.src, html);
+  gulp.watch(paths.imgs.src, imgs);
+};
+
+const build = gulp.series(clean, gulp.parallel(script, style, html, imgs));
+
+export { script };
+export { style };
+export { html };
+export { imgs };
+export { clean };
+export { watch };
+
+export default build;
